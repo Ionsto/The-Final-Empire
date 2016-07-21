05ed277463dc9733ecccf9ad7b146342c7014164
sam <am.sutcliffe2@gmail.com>
2016-07-21 14:31:10 +0100

    Updated code - removed maint

--- The Final Empire/Lua/Functions.lua
+++ The Final Empire/Lua/Functions.lua
@@ -6,14 +6,23 @@
 --When a road is created check the location,if it is on a river, then add gold per turn for the civ
 --When the road is destroyed, remove this extra gold per turn
 --
+GoldPlayers = {};
+for i = 0, GameDefines.MAX_MAJOR_CIVS - 1 do
+	if Players[i]:IsEverAlive() then
+		GoldPlayers[i] = 0;
+	end
+end
 --int hexX, int hexY, PlayerID player, int routeType
 function AddRoad(hexX, hexY, player, routeType)
+	GoldPlayers[player] += 1;
 end
 --int hexX, int hexY
 function DestroyRoad(hexX,hexY)
-
+	GoldPlayers[Map.GetPlot(hexX,hexY).GetPlayer()] -= 1;
 end
-function UpdateGold()
-
+function UpdateGold(player)
+	Players[player].ChangeGold(GoldPlayers[player]);
 end
 LuaEvents.SerialEventRoadCreated.Add(AddRoad);
+LuaEvents.SerialEventRoadDestroyed.Add(AddRoad);
+GameEvents.PlayerDoTurn.Add(UpdateGold);
